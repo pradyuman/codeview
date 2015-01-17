71aaf2528bd235748bf8634921b9172cddfaadcd
Scyphi <pradyumanvig@hotmail.com>
2015-01-17 18:17:35 -0500

    edits to app.js

--- public/app.js
+++ public/app.js
@@ -17,6 +17,34 @@ angular.module('codeview', ['ui.router', 'firebase', 'ui.ace'])
 
 .controller('HomeCtrl', function($scope) {
   $scope.test = 'asdf';
+  // Closes the sidebar menu
+$("#menu-close").click(function(hi) {
+    hi.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(hi) {
+    hi.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
 })
 
 .controller('CodeCtrl', function($scope, $stateParams, Room) {
@@ -61,33 +89,4 @@ angular.module('codeview', ['ui.router', 'firebase', 'ui.ace'])
     var ref = new Firebase("https://codeview1.firebaseio.com/rooms/").child(room);
     return $firebase(ref).$asObject();
   }
-});
-
-// Closes the sidebar menu
-$("#menu-close").click(function(hi) {
-    hi.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-});
-
-// Opens the sidebar menu
-$("#menu-toggle").click(function(hi) {
-    hi.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-});
-
-// Scrolls to the selected menu item on the page
-$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
 });
