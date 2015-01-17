9b5e9be85bca1540f91f2264ce2bbce273a3d0b8
Scyphi <pradyumanvig@hotmail.com>
2015-01-17 18:12:30 -0500

    app.js update

--- public/app.js
+++ public/app.js
@@ -62,3 +62,32 @@ angular.module('codeview', ['ui.router', 'firebase', 'ui.ace'])
     return $firebase(ref).$asObject();
   }
 });
+
+// Closes the sidebar menu
+$("#menu-close").click(function(hi) {
+    hi.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(hi) {
+    hi.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
