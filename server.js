c1d0804fb26f88dc550f9a427cc59368b05c440a
Ian Macalinao <me@ian.pw>
2015-01-17 03:09:47 -0600

    Add server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port', port);
+});
