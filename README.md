1936ffde2d1dc1fd13a4576f75eec3faa80e846e
Ian Macalinao <me@ian.pw>
2015-01-17 03:05:17 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+codeview
+===============
+
+Crowdsourced coding interview practice and preparation
+
+## Installing
+
+```bash
+$ npm install --save codeview
+```
+
+## License
+
+MIT
---
+++ test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var func = require('./');
+
+describe('codeview', function() {
+  it('should ...', function() {
+    expect(true).to.be.true;
+  });
+});
