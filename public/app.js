1a86757e15592572abb1a4fbb8ddca8062ef04d9
Ian Macalinao <me@ian.pw>
2015-01-17 23:07:15 -0600

    App

--- public/app.js
+++ public/app.js
@@ -45,69 +45,53 @@ angular.module('codeview', ['ui.router', 'firebase', 'ui.ace'])
       }
     });
   });
-	
-	$("#validatedForm").submit(function(event) {
 
-	var error = 0;
+  $("#validatedForm").submit(function(event) {
 
-	clearerror("#erroremail");
-	clearerror("#errorpassword");
+    var error = 0;
 
-	event.preventDefault();
+    clearerror("#erroremail");
+    clearerror("#errorpassword");
 
-	/*if (!isValidEmailAddress($("#email").val())) {
-		
-		$("#erroremail").html("The email address provided is invalid");
-		styleerror("#erroremail");
-		error = 1
-		
-	} */
-	
-	/*if (!isValidPassword($("#password").val())) {
-		
-		$("#errorpassword").html("The password provided is invalid");
-		styleerror("#errorpassword");
-		error = 1*/
-		
-	}
+    event.preventDefault();
 
-	if (error != 1) {
-		
-		$("#submitButton").animate({
-			"width":"240px"
-		},800);
-		$("#submitButton").val("Submitted successfully");
-		
-	}
+    if (error != 1) {
 
-	function isValidEmailAddress(emailAddress) {
-	var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-	return pattern.test(emailAddress);
-	};
-	
-	function isValidPassword(password) {
-	var pattern = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/);
-	return pattern.test(password);
-	};
+      $("#submitButton").animate({
+        "width": "240px"
+      }, 800);
+      $("#submitButton").val("Submitted successfully");
 
-	function styleerror(errortype) {
-		$(errortype).css("backgroundColor", "#CC6666");
-		$(errortype).css("width", "240px");
-		$(errortype).css("height", "20px");
-		$(errortype).css("marginBottom", "10px");
-		$(errortype).css("padding", "0px 5px 5px 15px");
-	}
+    }
 
-	function clearerror(errortype) {
-		$(errortype).html("");
-		$(errortype).css("backgroundColor", "none");
-		$(errortype).css("width", "0");
-		$(errortype).css("height", "0");
-		$(errortype).css("marginBottom", "0");
-		$(errortype).css("padding", "0");
-	}
+    function isValidEmailAddress(emailAddress) {
+      var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+      return pattern.test(emailAddress);
+    };
 
-	});
+    function isValidPassword(password) {
+      var pattern = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/);
+      return pattern.test(password);
+    };
+
+    function styleerror(errortype) {
+      $(errortype).css("backgroundColor", "#CC6666");
+      $(errortype).css("width", "240px");
+      $(errortype).css("height", "20px");
+      $(errortype).css("marginBottom", "10px");
+      $(errortype).css("padding", "0px 5px 5px 15px");
+    }
+
+    function clearerror(errortype) {
+      $(errortype).html("");
+      $(errortype).css("backgroundColor", "none");
+      $(errortype).css("width", "0");
+      $(errortype).css("height", "0");
+      $(errortype).css("marginBottom", "0");
+      $(errortype).css("padding", "0");
+    }
+
+  });
 })
 
 .controller('CodeCtrl', function($scope, $stateParams, Room, $http) {
