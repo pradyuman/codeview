fba16a0ffac53b7b3b6ece58a2df9053c478f9d9
Ian Macalinao <me@ian.pw>
2015-01-17 18:31:55 -0600

    Add execute path

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 var express = require('express');
+var evalin = require('evalin');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+app.post('/execute', function(req, res) {
+  var code = req.data.code;
+  var lang = req.data.lang;
+
+  if (!code) {
+    return res.json({
+      error: 'No code given.'
+    });
+  }
+
+  if (!lang) {
+    return res.json({
+      error: 'No language specified.'
+    });
+  }
+
+  evalin(code, lang).then(function(data) {
+    return res.json(data);
+  }).catch(function(e) {
+    return res.json({
+      error: 'Error evaluating code: ' + e.toString()
+    });
+  })
+
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
