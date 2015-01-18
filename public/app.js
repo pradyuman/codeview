b5ea22ec935871d1efe6b997a9aa11d3df13f4d1
Scyphi <pradyumanvig@hotmail.com>
2015-01-17 23:27:30 -0500

    Preliminary password validation
    client side

--- public/app.js
+++ public/app.js
@@ -45,6 +45,64 @@ angular.module('codeview', ['ui.router', 'firebase', 'ui.ace'])
       }
     });
   });
+	
+	$("#validatedForm").submit(function(event) {
+
+	var error = 0;
+
+	clearerror("#erroremail");
+	clearerror("#errorpassword");
+
+	event.preventDefault();
+
+	if (!isValidEmailAddress($("#email").val())) {
+		
+		$("#erroremail").html("The email address provided is invalid");
+		styleerror("#erroremail");
+		error = 1
+		
+	}
+
+	if($("#password").val() != $("#confirm").val()) {
+
+		$("#errorpassword").html("The passwords do not match");
+		styleerror("#errorpassword");
+		error=1;
+		
+	}
+
+	if (error != 1) {
+		
+		$("#submitButton").animate({
+			"width":"250px"
+		},800);
+		$("#submitButton").val("Submitted successfully");
+		
+	}
+
+	function isValidEmailAddress(emailAddress) {
+	var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+	return pattern.test(emailAddress);
+	};
+
+	function styleerror(errortype) {
+		$(errortype).css("backgroundColor", "#CC6666");
+		$(errortype).css("width", "280px");
+		$(errortype).css("height", "20px");
+		$(errortype).css("marginBottom", "10px");
+		$(errortype).css("padding", "5px 5px 5px 15px");
+	}
+
+	function clearerror(errortype) {
+		$(errortype).html("");
+		$(errortype).css("backgroundColor", "none");
+		$(errortype).css("width", "0");
+		$(errortype).css("height", "0");
+		$(errortype).css("marginBottom", "0");
+		$(errortype).css("padding", "0");
+	}
+
+	});
 })
 
 .controller('CodeCtrl', function($scope, $stateParams, Room, $http) {
