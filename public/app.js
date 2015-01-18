042496a769f4bf20b8ca919b0e11819355ced548
Scyphi <pradyumanvig@hotmail.com>
2015-01-17 23:42:55 -0500

    More optimizations
    This is probably going to work better for signup...

--- public/app.js
+++ public/app.js
@@ -62,19 +62,19 @@ angular.module('codeview', ['ui.router', 'firebase', 'ui.ace'])
 		error = 1
 		
 	}
-
-	if($("#password").val() != $("#confirm").val()) {
-
-		$("#errorpassword").html("The passwords do not match");
+	
+	if (!isValidPassword($("#password").val())) {
+		
+		$("#errorpassword").html("The password provided is invalid");
 		styleerror("#errorpassword");
-		error=1;
+		error = 1
 		
 	}
 
 	if (error != 1) {
 		
 		$("#submitButton").animate({
-			"width":"250px"
+			"width":"240px"
 		},800);
 		$("#submitButton").val("Submitted successfully");
 		
@@ -84,10 +84,15 @@ angular.module('codeview', ['ui.router', 'firebase', 'ui.ace'])
 	var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 	return pattern.test(emailAddress);
 	};
+	
+	function isValidPassword(password) {
+	var pattern = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/);
+	return pattern.test(password);
+	};
 
 	function styleerror(errortype) {
 		$(errortype).css("backgroundColor", "#CC6666");
-		$(errortype).css("width", "280px");
+		$(errortype).css("width", "240px");
 		$(errortype).css("height", "20px");
 		$(errortype).css("marginBottom", "10px");
 		$(errortype).css("padding", "5px 5px 5px 15px");
